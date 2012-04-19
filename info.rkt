3ba7966aa2de055aed84b15ac8eaec4253a709d2
Danny Yoo <dyoo@cs.wpi.edu>
2012-04-19 13:08:16 -0400

    info.ss to info.rkt

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang setup/infotab
+(define name "Aho-Corasick Tools")
+(define categories '(datastructures))
+(define doc.txt "doc.txt")
+(define blurb
+  (list "Aho-Corasick Tools: both an interpreter and a compiler "
+        "for Aho-Corasick automata"))
